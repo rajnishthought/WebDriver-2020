2ce959f4b488847abe1cbb8dbd58eeb3a2955fd4
Rajnish <rajnishgne@gmail.com>
2020-07-01 16:44:02 +0530

    webdriver setup

---
+++ src/main/java/Selenium2020/WebDriver_Selenium2020/App.java
@@ -0,0 +1,13 @@
+package Selenium2020.WebDriver_Selenium2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
